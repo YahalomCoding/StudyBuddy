85bf4c790668f95a1366dae0933f4913fe70b9b7
its-elad <59926027+its-elad@users.noreply.github.com>
2026-01-16 18:03:10 +0200

    feat: added validation pipes and nestjs-zod as well as the schemas and utils libs to api app

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
-export const exampleSchema = z.object({
+export const getExampleSchema = z.object({
   title: z.string(),
   description: z.string().optional(),
 });
-export type Example = z.infer<typeof exampleSchema>;
+export type Example = z.infer<typeof getExampleSchema>;
